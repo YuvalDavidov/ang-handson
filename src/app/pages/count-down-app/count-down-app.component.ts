e9345e2e9d39a96ccbe4e0c91058a9bcfdd7436a
Yuval <yuval838@gmail.com>
2023-02-17 12:29:53 +0200

    count-down done

--- src/app/pages/count-down-app/count-down-app.component.ts
+++ src/app/pages/count-down-app/count-down-app.component.ts
@@ -9,24 +9,28 @@ export class CountDownAppComponent implements OnInit {
   @Input() futureTime!: number
 
   // futureTime: number = Date.now() + 1000 * 60
-  timeToShow: string = ''
+  timeToShow: string = '01:00'
   diff: number = 0
   interval: any
 
   ngOnInit(): void {
     this.diff = this.futureTime - this.now
-    this.getTimer()
     this.interval = setInterval(() => {
       this.getTimer()
       this.diff = this.diff - 1000
-    }, 1000)
+    }, 100)
   }
 
   getTimer() {
-    if (this.diff > 59000) this.timeToShow = '01:00'
+    console.log(this.diff);
+
+    if (Math.floor(this.diff / 1000) > 59000) this.timeToShow = '01:00'
     else if (this.diff < 10000) this.timeToShow = '00:0' + Math.floor(this.diff / 1000)
     else this.timeToShow = '00:' + Math.floor(this.diff / 1000)
-    if (this.diff < 1000) clearInterval(this.interval)
+    if (this.diff < 1000) {
+      clearInterval(this.interval)
+      new Audio("../../../assets/audio/ding.mp3").play();
+    }
   }
 
 }
